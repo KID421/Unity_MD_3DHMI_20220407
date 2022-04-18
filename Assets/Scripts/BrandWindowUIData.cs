5e8e76bb3dd8674427f907315ee93665a9ce8fb1
KID <50666207+KID421@users.noreply.github.com>
2022-04-18 11:59:11 +0800

    Update 添加 URP 以及視窗切換第一種方式

---
+++ Assets/Scripts/BrandWindowUIData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// 各品牌視窗介面資料
+/// 1. 底圖顏色
+/// 2. 標題大小、字型
+/// 3. 內文顏色
+/// 4. 單選顏色
+/// 5. 關閉圖片
+/// </summary>
+public class BrandWindowUIData : MonoBehaviour
+{
+
+}
---
+++ Assets/Scripts/WindowUIManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+/// <summary>
+/// 視窗介面管理器
+/// </summary>
+public class WindowUIManager : MonoBehaviour
+{
+	/// <summary>
+	/// 品牌類型
+	/// </summary>
+	public enum Brand
+	{
+		BrandA, BrandB
+	}
+
+	[SerializeField, Header("品牌類型")]
+	private Brand brand;
+	[SerializeField, Header("彈出視窗物件")]
+	private GameObject[] windows;
+
+	/// <summary>
+	/// 開始事件：播放後執行一次
+	/// </summary>
+	private void Start()
+	{
+		// 視窗物件[品牌編號].啟動設定(顯示) - true 顯示，false 隱藏
+		windows[(int)brand].SetActive(true);
+	}
+}
